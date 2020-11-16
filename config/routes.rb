4199b11515f0f0c72e9f26a0efac26c476ed48be
Milena Grncarova <milenagrncarov@outlook.com>
2020-11-16 17:21:48 +0100

    Created BookingsModel/Controller/View

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :trucks do
+    resources :bookings, only: [ :new, :create, :index ]
+  end
+
 end
