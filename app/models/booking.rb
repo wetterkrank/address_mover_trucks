19cf1860fb9837954f62d45846abfe5653df7537
Milena Grncarova <milenagrncarov@outlook.com>
2020-11-17 15:37:16 +0100

    Draft

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,6 @@
 class Booking < ApplicationRecord
     validates :status, :start_date, :end_date, presence: true
+
+    belongs_to :user
+    belongs_to :truck
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :trucks do
-    resources :bookings, only: [ :new, :create, :index ]
+    resources :bookings, only: [ :new, :create, :index, :edit, :update ]
   end
 
 end
