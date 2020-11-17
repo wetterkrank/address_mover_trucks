6bbc833b69e867c3a6f46b7dab41ae3d6080a23b
Christoph Harlander <c.harlander@hotmail.com>
2020-11-17 17:04:59 +0100

    Implemented Routes, Controller, Views (without wishlist features)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'trucks/show'
+  get 'trucks/new'
+  get 'trucks/create'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   resources :trucks do
     resources :bookings, only: [ :new, :create, :index ]
   end
-
 end
