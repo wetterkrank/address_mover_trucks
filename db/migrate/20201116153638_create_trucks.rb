a5ff09db25b9f305f16ae1c2e523187b1e941c42
Christoph Harlander <c.harlander@hotmail.com>
2020-11-17 12:54:40 +0100

    Model created, validations defined, seeds established

--- db/migrate/20201116153638_create_trucks.rb
+++ db/migrate/20201116153638_create_trucks.rb
@@ -1,11 +1,11 @@
 class CreateTrucks < ActiveRecord::Migration[6.0]
   def change
     create_table :trucks do |t|
-      t.title :title
+      t.string :title
       t.string :location
       t.integer :price_per_day
       t.string :size
-      t.title :description
+      t.text :description
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Cleaning database now...'
+Truck.destroy_all
+puts 'Database clean ✅'
+
+10.times do 
+  Truck.create!(
+    title: Faker::Vehicle.make_and_model,
+    location: ['Berlin', 'Hamburg', 'Munich'].sample,
+    size: ['Large SUV', 'Pick-Up Truck', 'Small Van', 'Large Van'].sample,
+    price_per_day: rand(25..100),
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+  )
+end
+
+puts "Done! Created #{Truck.count} trucks."
