02685e6cc899db4333e68c0ec467838846583fb0
Milena Grncarova <milenagrncarov@outlook.com>
2020-11-17 15:58:14 +0100

    Booking references fix

--- db/migrate/20201116154118_create_bookings.rb
+++ db/migrate/20201116154118_create_bookings.rb
@@ -4,8 +4,8 @@ class CreateBookings < ActiveRecord::Migration[6.0]
       t.date :start_date
       t.date :end_date
       t.string :status
-      t.references :trucks, null: false, foreign_key: true
-      t.references :users, null: false, foreign_key: true
+      t.references :truck, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
