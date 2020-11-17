ce60d78e81d9ddee2a176b9864f89480b3f8a7aa
Christoph Harlander <c.harlander@hotmail.com>
2020-11-17 14:59:35 +0100

    Update on references

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # has_many :trucks
-  # has_many :bookings
+  has_many :trucks
+  has_many :bookings
   
 end
