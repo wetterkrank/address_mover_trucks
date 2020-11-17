18aa1cfde099dd5d8faffe5fe01ab94d8f597994
miluska schaeffer <miluska.schaeffer@gmail.com>
2020-11-17 14:18:35 +0100

    added navbar for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
