29de1dc31805853cae86631abbc268dd2ef45ac3
Milena Grncarova <milenagrncarov@outlook.com>
2020-11-19 14:35:44 +0100

    Added a map

--- Gemfile
+++ Gemfile
@@ -67,3 +67,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 # Added devise
 gem 'devise'
 gem 'pundit'
+
+#Geocoding
+gem 'geocoder'
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -10,4 +10,6 @@ has_many :bookings
   validates :size, presence: true
   validates :description, presence: true
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
